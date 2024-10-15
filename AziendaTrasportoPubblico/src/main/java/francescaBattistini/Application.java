138881feb9c17bb57b9e52c875fe2e71e57811d9
Francescabattistini <166622198+Francescabattistini@users.noreply.github.com>
2024-10-15 16:45:19 +0200

    modifiche

--- AziendaTrasportoPubblico/src/main/java/francescaBattistini/Application.java
+++ AziendaTrasportoPubblico/src/main/java/francescaBattistini/Application.java
@@ -6,7 +6,7 @@ import jakarta.persistence.Persistence;
 
 public class Application {
     //private static final
-    EntityManagerFactory emf = Persistence.createEntityManagerFactory("AziendaTrasportoPubblico");
+    //EntityManagerFactory emf = Persistence.createEntityManagerFactory("AziendaTrasportoPubblico");
     public static void main(String[] args) {
         //EntityManager em = emf.createEntityManager();
 
