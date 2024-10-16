90516eb8d685a55f1cd97e66a4d776a95a8c70a6
oBeta97 <paolopellizzari97@gmail.com>
2024-10-17 00:30:04 +0200

    Aggiunta menù di login/signin e inizializzazione classi MenuAdmin e MenuUtente

--- AziendaTrasportoPubblico/src/main/java/francescaBattistini/Application.java
+++ AziendaTrasportoPubblico/src/main/java/francescaBattistini/Application.java
@@ -1,71 +1,110 @@
 package francescaBattistini;
 
 import francescaBattistini.Enum.TipoUtente;
+import francescaBattistini.Exceptions.NotFoundException;
 import francescaBattistini.dao.BaseDAO;
 import francescaBattistini.entities.Utente;
+import francescaBattistini.menu.MenuAdmin;
+import francescaBattistini.menu.MenuUtente;
 import francescaBattistini.utils.Utils;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 
 import java.time.LocalDate;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
 public class Application {
 
     private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("AziendaTrasportoPubblico");
+    private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
         EntityManager em = emf.createEntityManager();
-        Scanner scanner = new Scanner(System.in);
 
         BaseDAO bd = new BaseDAO(em);
 
-        Utente utente = new Utente("Francesca", "Bauhaus", LocalDate.of(1992, 11, 13 ), TipoUtente.ABUSIVO);
+//        Utente utente = new Utente("Marco", "Vide", LocalDate.of(1992, 11, 13 ), TipoUtente.AMMINISTRATORE);
 
-        bd.save(utente);
+//        bd.save(utente);
 
-        System.out.println("FINE! :(");
+        System.out.println("BENBENUTO IN AUTOTRASPORTI BW6!");
+        boolean eRegistrato =  Utils.readYN("Sei già registrato?", scanner);
 
-        System.out.println("benvenuto");
-        boolean risposta =  Utils.readYN("sei già registrato?", scanner);
-
-        if (risposta){
-            System.out.println("login");
+        if (eRegistrato){
+            logInLoop:
             while (true) {
-                
-            }
-            System.out.println("1. login tramite ID");
-            System.out.println("2. mostrami lista utenti");
-            System.out.println("0. esci");
+                System.out.println(
+                        "1. Login tramite ID" + System.lineSeparator() +
+                        "2. Mostrami lista utenti" + System.lineSeparator() +
+                        "0. Esci dal software"
+                );
 
-            int command = Utils.readNumber("seleziona comando", scanner, 0,2);
-            switch (command) {
-                case 1:
-                    Utils.readString("inserisci l'ID", scanner);
-                    break;
-                case 2:
-                    List<Utente> utenti =  bd.getTakeAllObj(Utente.class);
-                    for(Utente u:utenti) {
-                        System.out.println(u);
-                    }
-                    break;
-                case 0:
-                    break;
-                default:
-                    System.out.println("comando non valido");
+                int command = Utils.readNumber("seleziona comando", scanner, 0,2);
+                switch (command) {
+                    case 1:
+                        String idUtente = Utils.readString("inserisci l'ID", scanner);
+                        try{
+                            Utente u = bd.getObjectById(Utente.class, idUtente);
+                            menuManager(u);
+                        } catch (NotFoundException e){
+                            System.out.println(e.getMessage());
+                        }
+                        break logInLoop;
+                    case 2:
+                        List<Utente> utenti =  bd.getTakeAllObj(Utente.class);
+                        for(Utente u:utenti) {
+                            System.out.println(u);
+                        }
+                        break;
+                    case 0:
+                        break logInLoop;
+                    default:
+                        System.out.println("comando non valido");
 
 
+                }
             }
         }
         else{
-            System.out.println("devi fare la registrazione");
+            System.out.println("SIGN IN: ");
+
+            Utente nuovoUtente;
+            if(Utils.readYN("Vuoi essere un utente anonimo?",scanner)) {
+                nuovoUtente = new Utente(TipoUtente.ANONIMO);
+            }
+            else{
+                String nome = Utils.readString("Inserisci il tuo nome:", scanner);
+                String cognome = Utils.readString("Inserisci il tuo cognome:", scanner);
+                LocalDate dataNascita = Utils.readDate("Inserisci la tua data di nascita: ", scanner);
+                TipoUtente tipoUtente = Utils.readEnum("Scegli il tuo ruolo: ", Arrays.stream(TipoUtente.values()).toList(), scanner);
+                nuovoUtente = new Utente(nome, cognome, dataNascita, tipoUtente);
+
+            }
+            bd.save(nuovoUtente);
+
+            menuManager(nuovoUtente);
         }
 
-        
-
-
-
+        System.out.println("FINE! :(");
+        scanner.close();
     }
+
+    private static void menuManager(Utente u){
+        switch (u.getTipologiaUtente()){
+            case TipoUtente.VIAGGIATORE:
+            case TipoUtente.ANONIMO:
+                MenuUtente.menuPrincipale(u, scanner);
+                break;
+            case TipoUtente.AMMINISTRATORE:
+                MenuAdmin.menuPrincipale(u, scanner);
+                break;
+            default:
+                System.out.println("Utente non autorizzato! Pussa via (╬ಠ益ಠ)");
+        }
+    }
+
+
 }
--- AziendaTrasportoPubblico/src/main/java/francescaBattistini/menu/MenuAdmin.java
+++ AziendaTrasportoPubblico/src/main/java/francescaBattistini/menu/MenuAdmin.java
@@ -1,4 +1,30 @@
 package francescaBattistini.menu;
 
+import francescaBattistini.entities.Utente;
+import francescaBattistini.utils.Utils;
+
+import java.util.Scanner;
+
 public class MenuAdmin {
+
+    public static void menuPrincipale(Utente u, Scanner s) {
+        System.out.println("Benvenuto" + (u.getName() != null ? " " + u.getName() : "") + "! Sei un utente " + u.getTipologiaUtente());
+
+        menuPrincipaleMainLoop:
+        while (true) {
+
+            System.out.println(
+                    "0. Esci dal software"
+            );
+
+            int command = Utils.readNumber("seleziona comando", s, 0, 1);
+
+            switch (command) {
+                case 0:
+                    break menuPrincipaleMainLoop;
+                default:
+                    System.out.println("Comando non valido");
+            }
+        }
+    }
 }
