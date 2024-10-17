0ab93b9eb16fda99bb1b6a363e47fe6bca549a4b
Francescabattistini <166622198+Francescabattistini@users.noreply.github.com>
2024-10-17 12:27:47 +0200

    primo commit

--- AziendaTrasportoPubblico/src/main/java/francescaBattistini/menu/MenuUtente.java
+++ AziendaTrasportoPubblico/src/main/java/francescaBattistini/menu/MenuUtente.java
@@ -14,7 +14,8 @@ public class MenuUtente {
         while(true){
 
             System.out.println(
-                    "0. Torna al menu principale"
+                    "1. hai la tessera? \n"
+                    
             );
 
             int command = Utils.readNumber("seleziona comando", s, 0,1);
@@ -28,6 +29,38 @@ public class MenuUtente {
         }
 
 
+        //1. Hai la tessera?
+
+        //2.SI/NO
+
+        //1.0 se SI dammi il numero della tessera.
+
+        //0.0 controllo tessera se è scaduta o se la vuole ****
+
+        //1.1  che cosa vuoi comprare(biglietto/abbonamento);
+        //1.2  Dove lo vuoi compare (distribbutore automatico[attivo,non attivo]/rivenditore );
+        //1.3  nel caso distribbutore automatico è ok va avanti(e ci ricaviamo il numero di biglietti/abbonamenti
+        // presenti nel count  nella tabella del distribbutore automatico);
+        //1.4 nel caso distribbutore automatico non è attivo ,"devi comprarlo dal riovenditore !");
+        //1.5 Nel caso nel rivenditore lo compri ( e ci ricaviamo il numero di biglietti/abbonamenti presenti
+        // e il punto di emissione dove è stato comprato);
+        //1.6 vuoi salire su TRAM o AUTOBUS?;
+        //1.7 Se l'Autobus o il Tram è in servizio e ha capienza entri;
+        //1.8 se l'Autobus o Tram non è in servizio Esce;
+        //1.9 e se l'autobus o il Tram è in servizio ma  non hanno capienza Esce;
+        //1.10 Il biglietto viene poi associato a quel determinato mezzo.;
+        //1.11 Entri nell'Autobus o Tram il biglietto cambia stato(biglietto Validato,Non validato);
+
+        //****  Se i biglietti ce li ha ma sono tutti annullati torna  punto 1.1
+        //abonamento se è scaduto (rinnovalo)torna a punto 1.1
+
+        //2.0 se la tessera è scaduta ?
+        //la vuoi rinnovare?(y,n)-se fosse no esce-
+        // se fosse Y
+        // 2.1
+        //se la vuole fare
+        // 2.1 prendiamo i dati dell'utente  e gli permettiamo di avere un codice tessera che avrò validità di un anno
+        // se no ESCE.
 
     }
 
